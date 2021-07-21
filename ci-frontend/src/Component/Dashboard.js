3c4b5d98b84dc1c5d7ff889c745e26d99e485b57
Jonathon Kindle <jonathonkindle@gmail.com>
2021-07-21 10:17:53 -0700

    added multiple components for login and dashboard as well as nav

---
+++ ci-frontend/src/Component/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Chart from './Chart';
+
+function Dashboard() {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <Chart />
+        </div>
+    )
+}
+
+export default Dashboard
