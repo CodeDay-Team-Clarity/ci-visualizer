77b0426ea52e1c7ac51ce30ed78230768bd5aa24
Turki BK <tbkaily.gmail.com>
2021-08-05 12:49:39 -0700

    creted sideNav

--- ci-frontend/src/Component/SideNav.js
+++ ci-frontend/src/Component/SideNav.js
@@ -1,9 +1,27 @@
+
 import React from 'react'
+import { SideNavData } from './SideNavData'
+import './SideData.css'
+
 
 function SideNav() {
     return (
-        <div>
-            
+        <div className='navSide'>
+            <ul className='navSideList'>
+               {SideNavData.map((val,key) =>{
+                   return(
+                
+
+             <li key={key} className='row' onClick={()=>{window.location.pathname=val.link;}}>
+
+
+                       <div>{val.ican}</div>
+                       <div>{val.title}</div>
+                   </li>
+
+
+                ) })}
+            </ul>
         </div>
     )
 }
