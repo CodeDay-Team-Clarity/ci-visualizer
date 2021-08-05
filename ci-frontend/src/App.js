4ba8a781f1e11043baa689aef031d54cb1cf930b
Jonathon Kindle <jonathonkindle@gmail.com>
2021-08-05 14:17:40 -0700

    fixed login, working on logout

--- ci-frontend/src/App.js
+++ ci-frontend/src/App.js
@@ -1,45 +1,28 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
-// import Login from './Component/Login';
+import Login from './Component/Login';
 import Dashboard from './Component/Dashboard';
 import Preferences from './Component/Preferences';
 
 
 function App() {
-  // const [token, setToken] = useState();
-
-  // if (!token) {
-  //   return <Login setToken = {setToken} />
-  // }
-
-  useEffect(() => {
-    const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ username: 'jenkins', password: 'codeday', url: 'http://builds.ci-visualizer.com:8080' })
-};
-    fetch('/login', requestOptions).then(
-      response => {
-        if (response.status === 200){
-          return response.json()
-        } else {
-          return {"message":"connection failed"}
-        }
-      }
-    ).then(data => console.log(data))
-  }, []);
-
   return (
     <div className = "wrapper">
       <h1>Application</h1>
       <BrowserRouter>
         <Switch>
-          <Route path="/stats">
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/dashboard">
             <Dashboard />
           </Route>
+          {/* <Route path="/stats">
+            <Dashboard />
+          </Route> */}
           <Route path="/preferences">
             <Preferences />
           </Route>
--- ci-frontend/src/Component/Login.js
+++ ci-frontend/src/Component/Login.js
@@ -1,28 +1,97 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import './Login.css';
 
-function Login() {
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    // use this for url for now: 'http://builds.ci-visualizer.com:8080'
+    const [url, setUrl] = useState("");
+
+    const login = async (username, password, url) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify({ 
+                'username': username, 
+                'password': password, 
+                'url': url 
+            })
+        };
+        
+        try{
+            const response = await fetch('/login', requestOptions)
+                if (response.status !== 200){
+                    alert("there has been a murder!!!");
+                    console.log({"message":"connection failed"});
+                    return false;
+                }
+            const data = await response.json();
+            console.log(data);
+            localStorage.setItem("loggedIn", true);
+            history.push("/dashboard");
+            return true;
+        }
+        catch(error){
+            console.error("There has been an error logging in")
+        }
+    };
+
+
+    const history = useHistory();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        login(username, password, url);
+    };
+
     return (
         <div className = "login-wrapper">
             <h1>Please Log In</h1>
-            <form>
-                <label>
-                    <p>Username</p>
-                    <input type = "text" />
-                </label>
-                <label>
-                    <p>password</p>
-                    <input type = "password" />
-                </label>
-                <label>
-                    <p>url</p>
-                    <input type = "url" />
-                </label>
-                <div>
-                    <button type = "submit">Submit</button>
-                </div>
-            </form>
+            {localStorage.getItem('loggedIn', true) ? (
+                history.push('/dashboard')
+                ) : (
+                <form>
+                    <label>
+                        <p>Username</p>
+                        <input 
+                            type = "text" 
+                            placeholder = "Username" 
+                            value = {username} 
+                            onChange = {(e) => setUsername(e.target.value)}
+                            />
+                    </label>
+                    <label>
+                        <p>password</p>
+                        <input
+                            type = "text" 
+                            placeholder = "Password" 
+                            value = {password} 
+                            onChange = {(e) => setPassword(e.target.value)}
+                            />
+                    </label>
+                    <label>
+                        <p>url</p>
+                        <input 
+                            type = "text" 
+                            placeholder = "Jenkins Url" 
+                            value = {url} 
+                            onChange = {(e) => setUrl(e.target.value)}
+                            />
+                    </label>
+                    <div>
+                        <button 
+                            type = "submit" 
+                            onClick = {handleClick} 
+                            >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+                )
+            }
         </div>
     )
 }
