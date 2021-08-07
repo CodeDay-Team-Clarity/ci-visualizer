f7c6688015ef3e1b42f8fbed0a617b1a3a7f8ebe
Jonathon Kindle <jonathonkindle@gmail.com>
2021-08-06 17:00:32 -0700

    added logout file and function

--- ci-frontend/src/Component/Dashboard.js
+++ ci-frontend/src/Component/Dashboard.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import Logout from './logout';
 import Chart from './Chart';
+
 
 function Dashboard() {
     return (
         <div>
+            <Logout />
             <h2>Dashboard</h2>
             <Chart />
         </div>
