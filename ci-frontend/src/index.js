d46d8ae88f786bafd23cb7ec1cdea505e97c9da3
Jonathon Kindle <jonathonkindle@gmail.com>
2021-08-10 21:33:42 -0700

    removed sass

--- ci-frontend/src/index.js
+++ ci-frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import bootstrap from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.esm.js';
-// import './Styles/index.css';
 import App from './App';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.esm.js';
+import './Styles/style.css';
+
+// import './Styles/index.css';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
