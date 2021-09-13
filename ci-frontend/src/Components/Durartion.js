2cdff472dd95ea0c3ca687ec556cf3f20cf9b576
Turki BK <tbkaily.gmail.com>
2021-09-13 10:45:02 -0700

    Adding background color to card

--- ci-frontend/src/Components/Durartion.js
+++ ci-frontend/src/Components/Durartion.js
@@ -4,38 +4,40 @@ import { Line } from "react-chartjs-2";
 // const [datas, setData] = [];
 // const [state, setstate] = useState(initialState);
 
-const data = {
-  labels: ["1", "2", "3", "4", "5", "6"],
-  datasets: [
-    {
-      label: "Duration",
-      data: [12, 19, 3, 5, 2, 3],
-      fill: false,
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgba(255, 99, 132, 0.2)",
-    },
-  ],
-};
-
-const options = {
-  scales: {
-    yAxes: [
+export default function Durartion(stats) {
+  const data = {
+    labels: [stats.BuildTimestamps],
+    // labels: ["1", "2", "3", "4", "5", "6"], // delet after backend works
+    datasets: [
       {
-        ticks: {
-          beginAtZero: true,
-        },
+        label: "Duration",
+        data: [stats.BuildDurations],
+        // data: [12, 19, 3, 5, 2, 3],  // delet after backend works
+        fill: false,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgba(255, 99, 132, 0.2)",
       },
     ],
-  },
-};
+  };
 
-const LineChart = () => (
-  <>
-    <div className="Duration">
-      <h1 className="title">Line Chart</h1>
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <div>
+      <>
+        <Line data={data} options={options} />
+      </>
+      `
     </div>
-    <Line data={data} options={options} />
-  </>
-);
-
-export default LineChart;
+  );
+}
