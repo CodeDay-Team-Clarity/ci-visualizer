615875cd0cdad6c4cd2cd0a717e866c12e2271d4
Turki BK <tbkaily.gmail.com>
2021-09-13 09:25:02 -0700

    Create job status

---
+++ ci-frontend/src/Components/JobsDuration.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function JobsDuration(stats) {
+  return (
+    <div>
+      <Card style={{ width: "15pc" }}>
+        <Card.Body>
+          <Card.Title>jobs data</Card.Title>
+          <Card.Text>
+            <h5>Number of Successes: {stats.Successes}</h5>
+            <h5>Number of Failures: {stats.Failures}</h5>
+            <h5>Number of Cancels: {stats.Cancels}</h5>
+            <h5>Average build time: {stats.Average}</h5>
+          </Card.Text>
+        </Card.Body>
+      </Card>{" "}
+    </div>
+  );
+}
