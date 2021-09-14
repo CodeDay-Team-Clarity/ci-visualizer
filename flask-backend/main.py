3043acb2ae5f273c7fc973155182819debfa8b3c
SamirIngley <samir.ingle7@gmail.com>
2021-09-13 20:18:44 -0700

    successful refactor jobs & stats routes

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -8,33 +8,92 @@ load_dotenv()
 
 app = Flask(__name__)
 
-class JenkinsConnection_WIP:
+class JenkinsCalls():
 
     def __init__(self, request=None):
         self.request = request
-    
-    def getJobs(self):
-        if self.request != None and (self.request.method == 'GET'):
-            args = self.request.args
-            print(args)
-            if "username" in args and "password" in args and "url" in args and "job" in args:
-                return JenkinsConnection(args["url"], args["username"], args["password"]), args["job"]
-            else:
-                raise Exception("Insufficient credentials")
-        else:
-            print('getJobs function : Invalid -> request = None (or POST instead of GET request)')
-    
-    def getStats(self):
-        if self.request != None and (self.request.method == 'GET'):
-            args = self.request.args
+
+    def doLogin(self):
+        # checks if logged in with POST method -> returns status code
+        if (self.request.method == 'POST'):
+            args = self.request.json
             print(args)
             if "username" in args and "password" in args and "url" in args:
                 return JenkinsConnection(args["url"], args["username"], args["password"])
             else:
                 raise Exception("Insufficient credentials")
+        return '{"response": "ok"}'
+    
+    def getAllJobs(self):
+        if self.request != None and (self.request.method == 'GET'):
+            args = self.request.args
+            print(args)
+            if "username" in args and "password" in args and "url" in args:
+                # Get jobs
+                connection = JenkinsConnection(args["url"], args["username"], args["password"])
+                jenkinsInstance = BuildMetrics(connection)
+
+                # consolidate all jobs, if no Jobs, return no jobs
+                allJobNames = jenkinsInstance.getJobNames()
+                if len(allJobNames) <= 0:
+                    return {"response": "no jobs"}
+
+                return {'allJobNames': allJobNames}
+            else:
+                raise Exception("Insufficient credentials Jobs")
+        else:
+            print('getJobs function : Invalid -> request = None (or POST instead of GET request)')
+    
+    def getJobStats(self):
+        if self.request != None and (self.request.method == 'GET'):
+            args = self.request.args
+            print(args)
+            if "username" in args and "password" in args and "url" in args and "job" in args:
+                # establish connection, establish current job if it's given in the args
+                connection = JenkinsConnection(args["url"], args["username"], args["password"])
+                currentJobName = None
+                if request.args["job"]:
+                    currentJobName = self.request.args["job"]
+                else: 
+                    return 'Job not specified getJobStats main.py'
+
+                # connect and get all jobs
+                jenkinsInstance = BuildMetrics(connection)
+
+                # all jobs, if no Jobs, return no jobs
+                allJobNames = jenkinsInstance.getJobNames()
+                if len(allJobNames) <= 0:
+                    error = {"response": "no jobs"}
+                    return error
+
+                jenkinsInstance.populateStats(currentJobName)
+                failures, successes, cancels = jenkinsInstance.getStatusCounts()
+                avgDuration, buildTimestamps, buildDurations, allResults = jenkinsInstance.getDurationTimeStatus()
+                # print(failures, successes, cancels, allResults, buildAvg)
+
+                data = {
+                    "allJobNames": allJobNames,
+                    "stats": {
+                        "CurrentJobName": currentJobName,
+                        "Failures": failures,
+                        "Successes": successes,
+                        "Cancels": cancels,
+                        "AverageDuration": avgDuration,
+                        "BuildTimestamps": buildTimestamps,
+                        "BuildDurations": buildDurations,
+                        "AllResults": allResults
+                    }
+                }
+
+                # return data
+                print('DATA ---- ')
+                print(data)
+                print(' -------- ')
+                return json.dumps(data)
+            else:
+                raise Exception("Insufficient credentials for getJobStats (user, pword, url)")
         else:
             print('getStats function : Invalid -> request=None (or POST instead of GET request)')
-
         
 def jenkinsConnectionFromRequest(request):
     if (request.method == 'GET'):
@@ -66,25 +125,16 @@ def loginRoute():
     jenkinsConnectionFromRequest(request)
     return '{"response": "ok"}'
 
-
 @app.route('/')
 def indexRoute():
-    return render_template("index.html", token="Hello, ci-visualizer user from Flask+React")
+    return "Hello, ci-visualizer user from Flask+React"
 
 
 @app.route('/jobs', methods=['GET'])
 def jobsRoute():
     ''' needs url, user, pwrd in the request '''
-    connection = jenkinsConnectionFromRequest(request)
-    jenkinsInstance = BuildMetrics(connection)
-
-    # all jobs, if no Jobs, return no jobs
-    allJobNames = jenkinsInstance.getJobNames()
-    if len(allJobNames) <= 0:
-        return {"response": "no jobs"}
-
-    return {'allJobNames': allJobNames}
-
+    connection = JenkinsCalls(request)
+    return connection.getAllJobs()
 
 @app.route('/stats', methods=['GET'])
 def statsRoute():
@@ -92,43 +142,44 @@ def statsRoute():
     in the query string, specify a username=, password=, url=, and job=
     Example: http://127.0.0.1:5000/stats?job=sleeper_simulation-1&username=jenkins&password=codeday&url=http://builds.ci-visualizer.com:8080/
     '''
-    currentJobName = None
+    # currentJobName = None
 
-    # connect and get all jobs
-    currentJobName = request.args["job"]
-    connection = jenkinsConnectionFromRequest(request)
-    jenkinsInstance = BuildMetrics(connection)
+    # # connect and get all jobs
+    # currentJobName = request.args["job"]
+    # connection = jenkinsConnectionFromRequest(request)
+    # jenkinsInstance = BuildMetrics(connection)
 
-    # all jobs, if no Jobs, return no jobs
-    allJobNames = jenkinsInstance.getJobNames()
-    if len(allJobNames) <= 0:
-        error = {"response": "no jobs"}
-        return error
+    # # all jobs, if no Jobs, return no jobs
+    # allJobNames = jenkinsInstance.getJobNames()
+    # if len(allJobNames) <= 0:
+    #     error = {"response": "no jobs"}
+    #     return error
 
-    jenkinsInstance.populateStats(currentJobName)
-    failures, successes, cancels = jenkinsInstance.getStatusCounts()
-    avgDuration, buildTimestamps, buildDurations, allResults = jenkinsInstance.getDurationTimeStatus()
-    # print(failures, successes, cancels, allResults, buildAvg)
+    # jenkinsInstance.populateStats(currentJobName)
+    # failures, successes, cancels = jenkinsInstance.getStatusCounts()
+    # avgDuration, buildTimestamps, buildDurations, allResults = jenkinsInstance.getDurationTimeStatus()
+    # # print(failures, successes, cancels, allResults, buildAvg)
 
-    data = {
-        "allJobNames": allJobNames,
-        "stats": {
-            "CurrentJobName": currentJobName,
-            "Failures": failures,
-            "Successes": successes,
-            "Cancels": cancels,
-            "AverageDuration": avgDuration,
-            "BuildTimestamps": buildTimestamps,
-            "BuildDurations": buildDurations,
-            "AllResults": allResults
-        }
-    }
+    # data = {
+    #     "allJobNames": allJobNames,
+    #     "stats": {
+    #         "CurrentJobName": currentJobName,
+    #         "Failures": failures,
+    #         "Successes": successes,
+    #         "Cancels": cancels,
+    #         "AverageDuration": avgDuration,
+    #         "BuildTimestamps": buildTimestamps,
+    #         "BuildDurations": buildDurations,
+    #         "AllResults": allResults
+    #     }
+    # }
 
-    # return data
-    print('DATA ---- ')
-    print(data)
-    print(' -------- ')
-    return json.dumps(data)
+    # # return data
+    # print('DATA ---- ')
+    # print(data)
+    # print(' -------- ')
+    connection = JenkinsCalls(request)
+    return connection.getJobStats()
 
 
 app.run(debug=True)
