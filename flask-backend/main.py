394dd6e9654b45ad3e4887d9b91ebbf2a42c8716
SamirIngley <samir.ingle7@gmail.com>
2021-09-13 20:25:05 -0700

    old code cleanup

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -94,92 +94,36 @@ class JenkinsCalls():
                 raise Exception("Insufficient credentials for getJobStats (user, pword, url)")
         else:
             print('getStats function : Invalid -> request=None (or POST instead of GET request)')
-        
-def jenkinsConnectionFromRequest(request):
-    if (request.method == 'GET'):
-        args = request.args
-        print(args)
-        # if a job is specified in the request, include the job name, else don't
-        if "username" in args and "password" in args and "url" in args and "job" in args:
-            return JenkinsConnection(args["url"], args["username"], args["password"])
-        else:
-            raise Exception("Insufficient credentials")
-            # return '{"response": "Error: Insufficient Credentials"}'
-    elif (request.method == 'POST'):
-        args = request.json
-        print(args)
-        if "username" in args and "password" in args and "url" in args:
-            return JenkinsConnection(args["url"], args["username"], args["password"])
-        else:
-            raise Exception("Insufficient credentials")
-            # return '{"response": "Error: Insufficient Credentials"}'
-    else:
-        raise Exception("No credentials")
-        # return '{"response": "Error: neither POST nor GET at jenkinsConnectionFromRequest"}'
-
 
 @app.route('/login', methods=['POST'])
 def loginRoute():
     # If this function call fails, the route will throw an exception, and the response won't have status code 200 i.e. login failed.
     # If this function call succeeds, the login succeeded, and we'll return a 200 status code with response body {"response": "ok"}
-    jenkinsConnectionFromRequest(request)
-    return '{"response": "ok"}'
+    connection = JenkinsCalls(request)
+    return connection.doLogin()
 
 @app.route('/')
 def indexRoute():
+    # Test connection to server
     return "Hello, ci-visualizer user from Flask+React"
-
 
 @app.route('/jobs', methods=['GET'])
 def jobsRoute():
-    ''' needs url, user, pwrd in the request '''
+    ''' Returns all jobs showing up on the jenkins server
+    needs url, user, pwrd in the request 
+    Example below
+    http://127.0.0.1:5000/jobs?username=jenkins&password=codeday&url=http://builds.ci-visualizer.com:8080/
+    '''
     connection = JenkinsCalls(request)
     return connection.getAllJobs()
 
 @app.route('/stats', methods=['GET'])
 def statsRoute():
     ''' Returns JSON of all build statistics of one job 
-    in the query string, specify a username=, password=, url=, and job=
-    Example: http://127.0.0.1:5000/stats?job=sleeper_simulation-1&username=jenkins&password=codeday&url=http://builds.ci-visualizer.com:8080/
+    Example below, in the query string, specify a username=, password=, url=, and job= 
+    http://127.0.0.1:5000/stats?job=sleeper_simulation-1&username=jenkins&password=codeday&url=http://builds.ci-visualizer.com:8080/
     '''
-    # currentJobName = None
-
-    # # connect and get all jobs
-    # currentJobName = request.args["job"]
-    # connection = jenkinsConnectionFromRequest(request)
-    # jenkinsInstance = BuildMetrics(connection)
-
-    # # all jobs, if no Jobs, return no jobs
-    # allJobNames = jenkinsInstance.getJobNames()
-    # if len(allJobNames) <= 0:
-    #     error = {"response": "no jobs"}
-    #     return error
-
-    # jenkinsInstance.populateStats(currentJobName)
-    # failures, successes, cancels = jenkinsInstance.getStatusCounts()
-    # avgDuration, buildTimestamps, buildDurations, allResults = jenkinsInstance.getDurationTimeStatus()
-    # # print(failures, successes, cancels, allResults, buildAvg)
-
-    # data = {
-    #     "allJobNames": allJobNames,
-    #     "stats": {
-    #         "CurrentJobName": currentJobName,
-    #         "Failures": failures,
-    #         "Successes": successes,
-    #         "Cancels": cancels,
-    #         "AverageDuration": avgDuration,
-    #         "BuildTimestamps": buildTimestamps,
-    #         "BuildDurations": buildDurations,
-    #         "AllResults": allResults
-    #     }
-    # }
-
-    # # return data
-    # print('DATA ---- ')
-    # print(data)
-    # print(' -------- ')
     connection = JenkinsCalls(request)
     return connection.getJobStats()
-
 
 app.run(debug=True)
