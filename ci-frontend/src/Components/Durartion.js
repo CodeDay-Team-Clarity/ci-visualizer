59e4092e1f678fb743e8de2521047314f1f38cd2
Turki BK <tbkaily.gmail.com>
2021-09-16 11:28:06 -0700

    add ing build time

--- ci-frontend/src/Components/Durartion.js
+++ ci-frontend/src/Components/Durartion.js
@@ -10,12 +10,12 @@ import { Line } from "react-chartjs-2";
 export default function Durartion({ stats }) {
   const data = {
     // labels: [stats.stats.BuildTimestamps],
-    labels: ["5", "2", "3", "4", "5", "6"], // delet after backend works
+    // labels: ["5", "2", "3", "4", "5", "6"], // delet after backend works
     datasets: [
       {
         label: "Duration",
-        data: [stats.stats.BuildDurations],
-        // data: [12, 19, 3, 5, 2, 3], // delet after backend works
+        // data: [stats.stats.BuildDurations],
+        data: [12, 19, 3, 5, 2, 3], // delet after backend works
         fill: false,
         backgroundColor: "rgb(255, 99, 132)",
         borderColor: "rgba(255, 99, 132, 0.2)",
