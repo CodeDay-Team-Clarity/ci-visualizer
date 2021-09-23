19f8166382e8307a7c4c865d62a88d98ae37aeed
Turki BK <tbkaily.gmail.com>
2021-09-23 10:52:37 -1000

    Fixed buges

--- ci-frontend/src/Components/Durartion.js
+++ ci-frontend/src/Components/Durartion.js
@@ -9,12 +9,12 @@ import { Line } from "react-chartjs-2";
 
 export default function Durartion({ stats }) {
   const data = {
-    // labels: [stats.stats.BuildTimestamps],
+    labels: [stats.durations.BuildTimestamps],
     // labels: ["5", "2", "3", "4", "5", "6"], // delet after backend works
     datasets: [
       {
         label: "Duration",
-        // data: [stats.stats.BuildDurations],
+        // data: [stats.durations.BuildDurations],
         data: [12, 19, 3, 5, 2, 3], // delet after backend works
         fill: false,
         backgroundColor: "rgb(255, 99, 132)",
